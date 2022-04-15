897f261eb299d789b294b8918f3d4d7ca841f3fe
fontofonto <donlam1994@gmail.com>
2022-04-14 17:12:03 -0700

    add Upvote component and icons

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,51 @@
-<template></template>
+<template>
+  <div class="row">
+    <div class="upvote-wrapper">
+      <Upvote />
+    </div>
+    <button class="add-btn">
+      <img
+        src="./assets/images/add-filled.svg"
+        alt="An arrow with the tail up"
+      />
+    </button>
+  </div>
+</template>
 
 <script>
+import Upvote from "./components/Upvote.vue";
 export default {
   name: "App",
-  components: {},
+  components: {
+    Upvote,
+  },
 };
 </script>
+
+<style scoped>
+.row {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+}
+.upvote-wrapper {
+  padding: 0.5rem;
+  border: 1px solid #dee2e6;
+  border-radius: 1rem;
+  flex: 1 1 auto;
+}
+button {
+  appearance: none;
+  border: none;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 48px;
+  height: 48px;
+  border-radius: 1rem;
+  cursor: pointer;
+}
+.add-btn {
+  margin: 1rem;
+}
+</style>
---
+++ src/components/Upvote.vue
@@ -0,0 +1,46 @@
+<template>
+  <button :class="{ 'is-selected': isSelected }" @click="toggleState">
+    <ArrowUp :fillColor="isSelected ? '#253CF2' : '#343A40'" />
+  </button>
+</template>
+
+<script>
+import { ref } from "vue";
+import ArrowUp from "./icons/ArrowUp.vue";
+
+export default {
+  components: {
+    ArrowUp,
+  },
+  setup() {
+    const isSelected = ref(false);
+
+    let toggleState = () => {
+      isSelected.value = !isSelected.value;
+    };
+
+    return {
+      isSelected,
+      toggleState,
+    };
+  },
+};
+</script>
+
+<style scoped>
+button {
+  appearance: none;
+  border: none;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 48px;
+  height: 48px;
+  border-radius: 1rem;
+  cursor: pointer;
+  background: #f4f6f8;
+}
+.is-selected {
+  background: #e5e8fd;
+}
+</style>
